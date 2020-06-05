1641d21cab36b2d797e0173761ea609d9aab5b2c
itepastra <itepastra@gmail.com>
2020-06-05 10:43:14 +0200

    Comments and better performance

--- Fraunhofer.py
+++ Fraunhofer.py
@@ -7,16 +7,33 @@ import readimgcolor
 
 fname = "test1.png"
 wavelength = 500e-9
-pixelsize = (0.05e-3, 0.05e-3)
-pixelamt = (1, 1)
+pixelsize = (0.05e-3, 0.5e-3)
+pixelamt = (5, 50)
 intensity = 1
 emitters = np.array([0, 0, 0, 0])
 observplanez = 3.05e0
 observplanesize = (5e-1, 5e-1)
 observres = (400, 400)
 cmap = mpl.cm.cool
+k = 2 * np.pi / wavelength
+c = 299792458
 
-def U
+B = lambda l, m, x, y, i: i * np.exp(1j * (k * c - k * (l * x + m * y)))
+C = lambda a, b: np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+
+def U(x, y, z):
+    co = np.array([x, y, z])
+    e1 = np.array([1, 0, 0])
+    e2 = np.array([0, 1, 0])
+    b = np.zeros_like(x, dtype="complex128")
+    print(len(emitters))
+    for n, emitter in enumerate(emitters):
+        b += B(C(co, e1), C(co, e2), emitter[0], emitter[1], emitter[2])
+        if n % 10 == 0:
+            print(n)
+    return b * pixelsize[0] / pixelamt[0] * pixelsize[1] / pixelamt[1]
+
 
 bw = readimgcolor.bwplaatje(fname)
 # create all emitter instances needed
@@ -31,7 +48,10 @@ for line in bw:
         xx, yy = np.meshgrid(xs, ys)
         x = np.ravel(xx)
         y = np.ravel(yy)
-        emitters = np.vstack((emitters, np.hstack((x, y, line[2] * intensity, 0))))
+        hstack = np.column_stack(
+            (x, y, np.full_like(x, line[2] * intensity), np.full_like(x, line[1]))
+        )
+        emitters = np.vstack((emitters, hstack))
 print(emitters)
 print(f"{len(emitters)} emitters have been created")
 
@@ -41,8 +61,8 @@ xs = np.linspace(-observplanesize[0] / 2, observplanesize[0] / 2, observres[0])
 ys = np.linspace(-observplanesize[1] / 2, observplanesize[1] / 2, observres[1])
 xx, yy = np.meshgrid(xs, ys)
 print(f"meshgrid of size {observres[0]} by {observres[1]} has been made")
-s = sum()
-I = np.abs(s)
+s = U(xx, yy, observplanez)
+I = np.real_if_close(s * np.conjugate(s))
 plt.pcolormesh(xx, yy, I, cmap=cmap)
 plt.colorbar()
 plt.show()
--- readimgcolor.py
+++ readimgcolor.py
@@ -11,16 +11,20 @@ def filepath(
 
 
 def bwplaatje(fname):
+    # hier openen we een plaatje
     im = Image.open(filepath(fname), "r")
     w, h = im.size
     print(w, h)
+    # we maken een numpy array van de pixelwaarden in het plaatje
     nim = np.array(im)
+    # waar een pixel rood genoeg is zien we als een opening in de slit
     r = np.argwhere(nim[:, :, 0] > 128)
+    # het blauwe deel van de pixel bepaalt de relatieve intensiteit op dat punt
     i = np.array([nim[r[:, 0], r[:, 1], 2]]).astype("float64").T / 255
+    # het groene deel doet op dit moment niets, maar deze zou kunnen worden gebruikt voor een faseverschuiving
     data = np.append(r.astype("float64"), i, 1)
-    print(data)
+    # hier draaien we de data om en maken we het midden van het plaatje het coordinaat (0,0)
     data[:, 0] -= (h - 1) / 2
     data[:, 1] -= (w - 1) / 2
     data[:, 0] *= -1
-    print(data)
     return data
